efa73e8a92b4e2d781e88a6a905939083c358840
Aidan Bjelke <aidan@ingu.co>
2019-06-12 22:44:16 -0600

    Added a main page work in progress

--- Website/index.php
+++ Website/index.php
@@ -1,6 +1,7 @@
 <html>
 <body>
 <h1>Sign-up</h1>
+<a href="mainpage.php">Home</a>
 <form action="add.php" method="post">
    E-mail: <input type="email" name="email"><br>
    Password: <input type="password" name="password"><br>
---
+++ Website/mainpage.php
@@ -0,0 +1,39 @@
+<html>
+<body>
+<h1>mysocial</h1>
+<a href="index.php">Login</a>
+
+<?php
+// Create connection
+$con=mysqli_connect("localhost","root","471-my-root","mysocial");
+
+// Check connection
+if (mysqli_connect_errno($con))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+$posts=mysqli_query($con,"SELECT * FROM POST");
+
+echo "<table border='1'>
+<tr>
+<th>Link</th>
+<th>Caption</th>
+<th>Email_address</th>
+</tr>";
+while($row = mysqli_fetch_array($posts))
+ {
+ echo "<tr>";
+ echo "<td>" . $row['Link'] . "</td>";
+ echo "<td>" . $row['Caption'] . "</td>";
+ echo "<td>" . $row['Email_address'] . "</td>";
+ echo "</tr>";
+ }
+echo "</table>";
+
+
+mysqli_close($con);
+?>
+
+</body>
+</html>
