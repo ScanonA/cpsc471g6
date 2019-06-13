f7193a34249219faab625a2ab7939f37299d52f3
Aidan Bjelke <aidan@ingu.co>
2019-06-12 21:13:43 -0600

    Added sample php files

---
+++ Website/index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>mysocial</h1>
+<form action="add.php" method="post">
+   E-mail: <input type="text" name="name"><br>
+   Password: <input type="text" name="email"><br>
+   <input type="submit" value="add">
+</form>
+
+</body>
+</html>
