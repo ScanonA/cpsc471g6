d2eb45ad9c2dba63e8c5063caa68d633a96e58d4
Aidan Bjelke <aidanbjelke@gmail.com>
2019-06-15 00:03:59 -0600

    Got most of the web page work done for commenting, but the comment table does not want to behave. Have to investigate why I can't even insert in phpmyadmin

--- Website/comment.php
+++ Website/comment.php
@@ -1,6 +1,6 @@
 <html>
 <body text="white" style="background-color:grey;">
-<h1>comment</h1>
+<h1>Comments</h1>
 <a href="index.php">Login</a>
 
 <?php
@@ -12,11 +12,27 @@ if (mysqli_connect_errno($con))
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
+  $currentlink = $_GET["link"];
+?>
 
-// $posts=mysqli_query($con,"INSERT INTO COMMENT (CText, Name, ID, Link) VALUE(%s, %s, %s, %s)");
-$currentlink = $_GET["link"];
+<form action="comment.php?link=".$currentlink method="get">
+   <input type='hidden' name='link' value='<?php echo "$currentlink";?>'/>
+   Comment: <input type="text" name="comment"><br>
+   <input type="submit" value="Submit">
+</form>
+
+<?php
+
+if(isset($_GET['comment']))
+{
+  $placeholder_name = 'bungle';
+  $placeholder_id = 789;
+  $my_comment = $_GET["comment"];
+  mysqli_query($con,"INSERT INTO COMMENT (CText, Name, ID, Link) VALUES('". $my_comment."','". $placeholder_name ."','". $placeholder_id ."','". $currentlink ."')");
+   echo "Comment: '". $my_comment ."' submitted!";
+}
+
 $comments=mysqli_query($con,"SELECT * FROM `comment` WHERE `Link` = '".$currentlink."'");
-
 echo "<table border='22'>
 <tr>
 <th>Comment</th>
