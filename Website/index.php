754eaec79ca925df1a2aff6cfbdbac4b40419d12
Aidan Bjelke <aidanbjelke@gmail.com>
2019-06-16 01:17:43 -0600

    Fixed posts and header changes

--- Website/index.php
+++ Website/index.php
@@ -1,16 +1,24 @@
-<?php session_start();?>
+<?php session_start();
 
-<html>
-<body text="white" style="background-color:grey;">
+echo "<html>
+<body text='white' style='background-color:grey;'>
 <h1>mysocial</h1>
-<form action = "search_thread.php" method = "post">
-  <input type = "text" name = "search" placeholder = "Search For Threads">
-  <input type = "submit" name = "submit search"> </button>
-</form>
-<a href="sign-up.php">Sign-up</a>
-<a href="login_page.php">Log-in</a>
+<a href='create_thread.php'>CreateThread</a>
+<form action = 'search_thread.php' method = 'post'>
+  <input type = 'text' name = 'search' placeholder = 'Search For Threads'>
+  <input type = 'submit' name = 'submit search'> </button>
+</form>";
+if(isset($_GET['command']) && $_GET['command'] == 'logout'){
+  session_unset();
+}
+if(!isset($_SESSION['email'])) {
+  echo "<a href='sign-up.php'>Sign-up </a>
+        <a href='login_page.php'>Log-in</a>";
+}
+if(isset($_SESSION['email'])) {
+  echo "<a href='index.php?command=logout&'>Log out</a>";
+}
 
-<?php
 // Create connection
 $con=mysqli_connect("localhost","root","471-my-root","mysocial");
 
@@ -20,16 +28,33 @@ if (mysqli_connect_errno($con))
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-if(!isset($_SESSION['email'])) {
+  ?>
+
+  <form action="index.php" method="get">
+     Caption: <input type="text" name="caption"><br>
+     Link: <input type="text" name="link"><br>
+     <input type="submit" value="Submit">
+  </form>
+  
+  <?php
+  if(isset($_GET['caption']) && isset($_GET['link']))
+  {
+    $my_link = $_GET['link'];
+    if(isset($_SESSION['email'])) {
+      mysqli_query($con,"INSERT INTO POST (Link, Caption, Email_address) VALUES('". $my_link."','". $_GET['caption'] ."','". $_SESSION['email'] ."')");
+    } else {
+      mysqli_query($con,"INSERT INTO POST (Link, Caption) VALUES('". $my_link."','". $_GET['caption'] ."')");
+    }
+    echo "Link: '". $my_link ."' submitted!";
+  }
+
+if(!isset($_SESSION['email'])) { // get posts here
   $posts=mysqli_query($con,"SELECT * FROM POST");
   $id = rand(100 , 999);
   $_SESSION['id'] = $id;
   $_SESSION['name'] = "anon";
   // echo "I love you Eena";
 } else {
-  ?>
-  <a href="create_thread.php">CreateThread</a>
-  <?php
   echo "<br>". "Welcome, ". $_SESSION['name'];
   $email = $_SESSION['email'];
   $posts=mysqli_query($con,"SELECT SUBSCRIBES_TO.Name, POST.Caption, CONTAINS.Link 
